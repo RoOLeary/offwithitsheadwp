51a75ac280c18f9ece42d02853b8c736693e6f0d
Pablo Roman <pablo@thenextweb.com>
2017-10-02 20:11:31 +0200

    Prevent rendering from failing when a post has no categories

--- src/scripts/components/PostList.js
+++ src/scripts/components/PostList.js
@@ -22,16 +22,20 @@ class PostList extends React.Component{
                             
                         <div className="single-post" key={i} value={post.id}>
                             <h3><Link to={`/posts/${post.slug}`}>{post.title.rendered}</Link></h3>
-                             <h5>Categories:</h5>
-                            <ul className="categories">
-                                {post.cats.map((cats, term_id) =>
-                                <li key={term_id}>
-                                    <Link to={`/posts/${post.cats[term_id].slug}`}>
-                                        {post.cats[term_id].name}
-                                    </Link>
-                                </li>
-                                )}
-                            </ul>
+                            {post.cats &&
+                                <div>
+                                    <h5>Categories:</h5>
+                                    <ul className="categories">
+                                        {post.cats.map((cats, term_id) =>
+                                        <li key={term_id}>
+                                            <Link to={`/posts/${post.cats[term_id].slug}`}>
+                                                {post.cats[term_id].name}
+                                            </Link>
+                                        </li>
+                                        )}
+                                    </ul>
+                                </div>
+                            }
                             <p>{post.date_gmt}</p>
                             <p dangerouslySetInnerHTML={{__html: post.excerpt.rendered}} />
                             <Link key={i} to={`/posts/${post.slug}`} style={{marginRight: '10px'}}>Go TO {post.title.rendered}</Link>
